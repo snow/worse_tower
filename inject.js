62b9548095bcbc822b3172ac3935469f64daf751
Snow Helsing <snow.helsing@gmail.com>
2015-09-28 23:51:34 +0800

    CHG: wrap header content in .inner

--- inject.js
+++ inject.js
@@ -10,3 +10,19 @@
   // ------------------
   chrome.runtime.sendMessage({});
 })();
+
+(function($){
+  $('.header > *').wrapAll('<div class="inner" />');
+  // window.addEventListener('popstate', function(evt){
+  //   console.log(evt);
+  // });
+
+  // $(window).on('popstate', function(evt){
+  //   console.log('> w <');
+  //   console.log(evt);
+  // });
+
+  // $(document).on('pjaxload#page-projects', function(evt){
+  //   console.log(evt);
+  // });
+})(jQuery);
