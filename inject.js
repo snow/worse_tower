95b079450109ff6d3cc03e5bf4d2fedc0bed5d4e
Snow Helsing <snow.helsing@gmail.com>
2016-02-07 23:13:46 +0800

    NEW: draft for busy panel

--- inject.js
+++ inject.js
@@ -45,6 +45,10 @@
       generateSidebar();
       pageId = currentId;
     }
+
+    if('page-members' == currentId) {
+      addBusyPanel();
+    }
   }, 2000);
 
   $(document).on('click', '.wtwr-sidebar a', function(e) {
@@ -57,5 +61,45 @@
 
     $('body, html').scrollTop(target.offset().top - 70) ;
   });
+
+  function addBusyPanel() {
+    if($('#page-members .switch-to-busy').length > 0) { return; }
+
+    var jSwitch = $('<a class="switch-to-busy" href="javascript:;">Busy Panel</a>');
+    var jMemberPanel = $('#page-members');
+    var jBusyPanel = $('<div id="page-busy" class="page-inner" data-page-name="Busy">'+
+      '<section class="today">' +
+      '<h5 class="box-title"><i class="twr twr-crosshairs"></i>今天</h5>' +
+      '<div class="todolist">' +
+      '<ul class="todos"></ul>'+
+      '</div>' +
+      '</section>' +
+      '<section class="next">' +
+      '<h5 class="box-title"><i class="twr twr-tasks"></i>接下来</h5>' +
+      '<div class="todolist">' +
+      '<ul class="todos"></ul>'+
+      '</div>' +
+      '</section>' +
+      '</div>')
+    $('#page-members .group-default').prepend(jSwitch);
+
+    jSwitch.on('click', function(evt){
+      jBusyPanel.insertBefore(jMemberPanel);
+      jMemberPanel.detach();
+    });
+
+    var connGuid = $('#conn-guid').val();
+    jMemberPanel.find('.member-link').each(function(idx, el){
+      var memberUrl = el.href + "?conn_guid="+connGuid+"&pjax=1";
+      $.ajax({
+        url: memberUrl,
+        success: function(data) {
+          var jPage = $(data)
+          jBusyPanel.find('.today ul').append(jPage.find('.box-today .todos .todo'));
+          jBusyPanel.find('.next ul').append(jPage.find('.box-next .todos .todo'));
+        }
+      });
+    });
+  }
 
 })(jQuery);
